4824a17e00fd6238d118d60f9e02c4ca69efa3ea
Rahmenordnung <43711100+Rahmenordnung@users.noreply.github.com>
2020-07-10 00:10:04 +0100

    Crud Funktionalität fertig etwa

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,8 @@
 class ArticlesController < ApplicationController
+before_action :set_article, only: [:show, :edit, :update, :destroy]
 
   def show
     
-    @article = Article.find(params[:id])
   end
   
   def index
@@ -15,12 +15,12 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
+    
   end 
   
   
   def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
     if @article.save
       flash[:notice] = "Artikel wurde erfolgreich erstellt"
       redirect_to @article
@@ -30,12 +30,28 @@ class ArticlesController < ApplicationController
   end
   
   def update
-      @article = Article.find(params[:id])
-      if @article.update(params.require(:article).permit(:title, :description))
+      if @article.update(article_params)
         flash[:notice] = "Artikel wurde erfolgreich bearbeitet"
       else
         render 'edit'
       end     
   end 
+
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+
+  end
+
+  private
+
+  def set_article
+  @article = Article.find(params[:id])
+
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end  
 
 end  
