a53000755a573822bc426a296aea1aab2b717d1d
Rahmenordnung <43711100+Rahmenordnung@users.noreply.github.com>
2020-07-09 15:41:02 +0100

    Verlegen das Kode implementiert

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,4 +9,33 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def new
+    @article = Article.new
+
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end 
+  
+  
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+      flash[:notice] = "Artikel wurde erfolgreich erstellt"
+      redirect_to @article
+    else
+      render 'new'
+    end 
+  end
+  
+  def update
+      @article = Article.find(params[:id])
+      if @article.update(params.require(:article).permit(:title, :description))
+        flash[:notice] = "Artikel wurde erfolgreich bearbeitet"
+      else
+        render 'edit'
+      end     
+  end 
+
 end  
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,7 @@
 class Article < ApplicationRecord
+  #validates :title, presence :true, length: { minimum: 6, maximum: 100 }
+  #validates :description, presence :true, length: { minimum: 10, maximum: 600 }
+  validates :title, presence: true, length: { minimum: 6, maximum: 100 }
+  validates :description, presence: true, length: { minimum: 10, maximum: 300 }
 
 end
